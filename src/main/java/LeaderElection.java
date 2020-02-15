b712c88feae0de708e46b2ffc42c24410b2b4e15
AlexF <farujoal1997@gmail.com>
2020-02-15 11:48:22 -0600

    Implementacion de la acciones en un znode <watchTargetZnode>

--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,28 +1,37 @@
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-
+/**
+ * Leader Election Implementation in Java
+ */
 public class LeaderElection implements Watcher {
-
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
     private static final String ELECTION_NAMESPACE = "/election";
+    private static final String TARGET_ZNODE = "/target_znode";
     private ZooKeeper zooKeeper;
     private String currentZnodeName;
+    private String parent;
 
-    public static void main(String [] args) throws IOException, InterruptedException, KeeperException {
-        LeaderElection lE = new LeaderElection();
-        lE.connectToZookeeper();
-        lE.volunteerForLeadership();
-        lE.electLeader();
-        lE.run();
-        lE.close();
+    public static void main(String[] arg) throws IOException, InterruptedException, KeeperException {
+        LeaderElection leaderElection = new LeaderElection();
+
+        leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
+        leaderElection.watchTargetZnode();
+        leaderElection.run();
+        leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application");
     }
 
+    /*public void creatParent() throws  KeeperException, InterruptedException{
+        String parent = zooKeeper.create("", new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+    }*/
     public void volunteerForLeadership() throws KeeperException, InterruptedException {
         String znodePrefix = ELECTION_NAMESPACE + "/c_";
         String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
@@ -45,20 +54,33 @@ public class LeaderElection implements Watcher {
         System.out.println("I am not the leader, " + smallestChild + " is the leader");
     }
 
+    public void watchTargetZnode() throws KeeperException, InterruptedException {
+        Stat stat = zooKeeper.exists(TARGET_ZNODE, this);
+        if (stat == null) {
+            return;
+        }
+
+        byte[] data = zooKeeper.getData(TARGET_ZNODE, this, stat);
+        List<String> children = zooKeeper.getChildren(TARGET_ZNODE, this);
+
+        System.out.println("Data : " + new String(data) + ", children : " + children);
+    }
+
+
     public void connectToZookeeper() throws IOException {
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    private void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
     }
 
     private void close() throws InterruptedException {
         this.zooKeeper.close();
     }
 
-
-    public void run() throws InterruptedException{
-        synchronized (zooKeeper){
-            zooKeeper.wait();
-        }
-    }
     @Override
     public void process(WatchedEvent event) {
         switch (event.getType()) {
@@ -71,8 +93,27 @@ public class LeaderElection implements Watcher {
                         zooKeeper.notifyAll();
                     }
                 }
+                break;
+            case NodeDeleted:
+                System.out.println(TARGET_ZNODE + " was deleted");
+                break;
+            case NodeCreated:
+                System.out.println(TARGET_ZNODE + " was created");
+                break;
+            case NodeDataChanged:
+                System.out.println(TARGET_ZNODE + " data changed");
+                break;
+            case NodeChildrenChanged:
+                System.out.println(TARGET_ZNODE + " children changed");
+                break;
+        }
+
+        try {
+            watchTargetZnode();
+        } catch (KeeperException e) {
+
+        } catch (InterruptedException e) {
+
         }
     }
-
-
 }
